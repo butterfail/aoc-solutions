19947a527d65204621ad706b3fd77ea21a3802a7
Grégoire Ciles <gregoire.ciles@protonmail.com>
2023-07-14 11:04:33 +0200

    Add 2022 - Day 4 - Camp Cleanup

---
+++ 2022/day-04/index.ts
@@ -0,0 +1,66 @@
+/**
+ * 2022 - Day 04
+ * https://adventofcode.com/2022/day/4
+ */
+
+import fs from 'node:fs'
+import path from 'node:path'
+
+console.clear()
+
+export type Input = string[]
+export type Filename = 'input' | 'input.test'
+
+const format = (filename: Filename): Input => {
+  return fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .replace(/\r/g, '')
+    .trim()
+    .split('\n')
+}
+
+/**
+ * Part 1
+ */
+export const part1 = (input: Input) => {
+  return input
+    .map(line => {
+      const [interval1, interval2] = line
+        .split(',')
+        .map(interval => interval.split('-').map(Number))
+        .sort((a, b) => {
+          const oneSize = a[1] - a[0]
+          const twoSize = b[1] - b[0]
+          return twoSize - oneSize
+        })
+
+      const oneFullContainsTwo = interval1[0] <= interval2[0] && interval1[1] >= interval2[1]
+
+      return oneFullContainsTwo ? 1 : 0
+    })
+    .reduce((acc, value) => acc + value, 0)
+}
+console.log('Part 1', part1(format('input.test')))
+
+/**
+ * Part 2
+ */
+export const part2 = (input: Input) => {
+  return input
+    .map(line => {
+      const [interval1, interval2] = line
+        .split(',')
+        .map(interval => interval.split('-').map(Number))
+        .sort((a, b) => {
+          const oneSize = a[1] - a[0]
+          const twoSize = b[1] - b[0]
+          return twoSize - oneSize
+        })
+
+      const isOverlapping = interval1[1] >= interval2[0] && interval1[1] >= interval2[0]
+
+      return isOverlapping ? 1 : 0
+    })
+    .reduce((acc, value) => acc + value, 0)
+}
+console.log('Part 2', part2(format('input.test')))
