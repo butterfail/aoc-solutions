1414e01e715fdf898355c753ed5af52f56d3a437
Grégoire Ciles <gregoire.ciles@protonmail.com>
2023-07-14 13:20:48 +0200

    Add 2022 - Day 5 - Supply Stacks

---
+++ 2022/day-05/index.ts
@@ -0,0 +1,101 @@
+/**
+ * XXXX - Day XX
+ * https://adventofcode.com/XXXX/day/X
+ */
+
+import fs from 'node:fs'
+import path from 'node:path'
+
+console.clear()
+
+export type Input = {
+  indexes: string[]
+  stacks: Record<string, string[]>
+  moves: {
+    count: number
+    from: number
+    to: number
+  }[]
+}
+export type Filename = 'input' | 'input.test'
+
+const format = (filename: Filename): Input => {
+  const [rawStacks, rawMoves] = fs
+    .readFileSync(path.resolve(__dirname, filename), 'utf8')
+    .replace(/\r/g, '')
+    .trimEnd()
+    .split('\n\n')
+    .map(x => x.split('\n'))
+
+  const parsedStacks = rawStacks.map(line => [...line].filter((_, idx) => idx % 4 === 1))
+
+  const indexes = parsedStacks.pop()
+
+  const stacks = {}
+  for (const line of parsedStacks) {
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] !== ' ') {
+        // Add line[i] to the stack indexes[i]
+        if (!stacks[indexes[i]]) {
+          stacks[indexes[i]] = []
+        }
+
+        stacks[indexes[i]].unshift(line[i])
+      }
+    }
+  }
+
+  const moves = []
+  for (const move of rawMoves) {
+    const match = /move (\d+) from (\d+) to (\d+)/g.exec(move)
+    moves.push({
+      count: parseInt(match[1]),
+      from: parseInt(match[2]),
+      to: parseInt(match[3])
+    })
+  }
+
+  return { indexes, stacks, moves }
+}
+
+/**
+ * Part 1
+ */
+export const part1 = ({ stacks, moves, indexes }: Input) => {
+  const stacksCloned = { ...stacks }
+
+  for (const move of moves) {
+    for (let i = 0; i < move.count; i++) {
+      const crate = stacksCloned[move.from].pop()
+      stacksCloned[move.to].push(crate)
+    }
+  }
+
+  return indexes
+    .map(value => {
+      const stack = stacksCloned[value]
+      return stack[stack.length - 1]
+    })
+    .join('')
+}
+console.log('Part 1', part1(format('input')))
+
+/**
+ * Part 2
+ */
+export const part2 = ({ stacks, moves, indexes }: Input) => {
+  const stacksCloned = { ...stacks }
+
+  for (const move of moves) {
+    const crates = stacksCloned[move.from].splice(-move.count, move.count)
+    stacksCloned[move.to] = stacksCloned[move.to].concat(crates)
+  }
+
+  return indexes
+    .map(value => {
+      const stack = stacksCloned[value]
+      return stack[stack.length - 1]
+    })
+    .join('')
+}
+console.log('Part 2', part2(format('input')))
